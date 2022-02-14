cbdf18488345d26d9dc3e6c24b145655b5dcb5c4
Jonathan Gilmer <jonathan@jonathangilmer.com>
2022-02-14 15:49:54 -0500

    Initial commit for project Selection Statements

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercise 1:
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+        }
+    }
+}
